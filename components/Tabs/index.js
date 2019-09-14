13324edef58bc96ff1f24908dc6b62e7c923fff3
rich92105 <52473445+rich92105@users.noreply.github.com>
2019-09-13 18:42:02 -0700

    completed head and tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,24 +8,45 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-const cards = document.querySelector(".cards");
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-  .then((results) => {
-    
-    const newcard =createCard(results.data);
-    
-    cards.appendChild(newcard);
+const topics = document.querySelector('.topics');
 
-    //Stretch Graph -- begin
-    const calendarDiv = document.createElement("div");
-    cards.appendChild(calendarDiv);
-    
-    calendarDiv.classList.add("calendar");
+function newTab(tabInfo){
+    // CREATE ELEMENTS
+    const tab = document.createElement('div');
 
-    new GitHubCalendar(".calendar", "richardmachado");
-    //Stretch Graph -- end
-    
-  })
-  .catch((err) => {
-    console.log(err);
-})
+    // ADD CLASSES
+    tab.classList.add('tab');
+
+    // ADD CONTENT
+    tab.textContent = `${tabInfo}`;
+
+    // APPEND
+
+    // RETURN
+    return tab;
+}
+
+    axios
+    // GET REQUEST
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    // PROMISE
+    .then(response => {
+        // RETURN RESPONSE
+        // console.log(response);
+        response.data.topics.forEach(items => {
+            // APPEND
+            topics.appendChild(newTab(items))
+        })
+    })
+    // CATCH ERRORS
+    .catch(error => {
+        console.log('The data was not received. Try again.', error)
+    })
+
+  /* START HERE: 
+  - Select all classes named ".tab" and assign that value to the tabs variable
+  - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
+  - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
+  */
+  
+  let tabs = document.querySelectorAll(".tab").forEach(item=>new TabLink(item));
