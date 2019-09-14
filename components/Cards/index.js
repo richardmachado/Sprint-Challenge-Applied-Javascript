b6a1ec661cbd95dadb9a7a9e4098703a92134302
rich92105 <52473445+rich92105@users.noreply.github.com>
2019-09-13 19:11:00 -0700

    i think it's done, couldn't figure out the stretch goal

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,66 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cards = document.querySelector('.cards-container');
+
+// INFO HERE IS WITHIN THE OBJECTS 
+function newCard(info) {
+    // CREATE ELEMENTS
+    const
+        card = document.createElement('div'),
+        cardHeadline = document.createElement('div'),
+        AuthorInfo = document.createElement('div'),
+        ImgContainer = document.createElement('div'),
+        Img = document.createElement('img'),
+        Author = document.createElement('span');
+
+    // ADD CLASSES
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    AuthorInfo.classList.add('author');
+    ImgContainer.classList.add('img-container');
+
+    // ADD CONTENT
+    cardHeadline.textContent = `${info.headline}`;
+    Img.src = `${info.authorPhoto}`;
+    Author.textContent = `By ${info.authorName}`;
+
+    // APPEND
+    card.appendChild(cardHeadline);
+    card.appendChild(AuthorInfo);
+    AuthorInfo.appendChild(ImgContainer);
+    ImgContainer.appendChild(Img);
+    AuthorInfo.appendChild(Author);
+
+    
+    // RETURN
+    return card;
+}
+
+// USE .FOREACH FOR THE ARRAY (RESPONSE>DATA>ARTICLES>ITEMS[ARRAY]>INFO{OBJECTS})
+// USE THIS. TO PULL THE ARRAY'S OBJECTS
+// IF .FOREACH AXIOS -> CREATE NAME FOR ARRAY
+// IF .FOREACH WITHIN AXIOS CONTAINING .THEN -> GET TO ITEMS
+
+
+// GET REQUEST
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+
+    // PROMISE
+    .then(response => {
+
+        const obj = Object.keys(response.data.articles);
+        for (let i = 0; i < obj.length; i++) {
+            response.data.articles[obj[i]].forEach(item => {
+                cards.appendChild(newCard(item))
+            })
+        }
+
+
+    })
+    // CATCH ERRORS
+    .catch(error => {
+        console.log('ERROR with the data.', error)
+    })
+    
